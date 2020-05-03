2432489834da2d2fd75c7122bf97e301ea066b62
Adnane Mane <tim@timrich.org>
2020-05-03 21:55:53 +0000

    tracking drive

---
+++ navigation/index.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { createAppContainer } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
+
+import Welcome from "../screens/Welcome";
+import Rewards from "../screens/Rewards";
+import Trip from "../screens/Trip";
+
+import { theme } from "../constants";
+
+const screens = createStackNavigator(
+  {
+    Welcome,
+    Rewards,
+    Trip
+  },
+  {
+    defaultNavigationOptions: {
+      headerStyle: {
+        height: 60,
+        backgroundColor: theme.colors.gray4,
+        borderBottomColor: "transparent"
+      },
+      headerTitleContainerStyle: {
+        alignItems: "flex-end",
+        paddingLeft: theme.sizes.padding
+      },
+      headerLeftContainerStyle: {
+        alignItems: "flex-end",
+        marginLeft: theme.sizes.padding,
+        paddingRight: theme.sizes.base
+      },
+      headerRightContainerStyle: {
+        alignItems: "flex-end",
+        marginRight: theme.sizes.padding
+      }
+    },
+    headerLayoutPreset: "left"
+  }
+);
+
+export default createAppContainer(screens);
